a95aa86bae1a81023fff35d85014833860489de1
Jerome <marhic.jerome@gmail.com>
2025-03-09 09:59:57 +0900

    add cloudfront behavior

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -5,7 +5,7 @@ import * as ecspatterns from 'aws-cdk-lib/aws-ecs-patterns';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
-import { S3StaticWebsiteOrigin } from 'aws-cdk-lib/aws-cloudfront-origins';
+import { S3StaticWebsiteOrigin, LoadBalancerV2Origin } from 'aws-cdk-lib/aws-cloudfront-origins';
 import * as iam from 'aws-cdk-lib/aws-iam';
 
 export class CdkStack extends cdk.Stack {
@@ -23,46 +23,36 @@ export class CdkStack extends cdk.Stack {
     });
 
     // Define the Fargate service
-    // See https://docs.aws.amazon.com/cdk/api/v2/docs/aws-cdk-lib.aws_ecs_patterns.ApplicationLoadBalancedFargateService.html
     const fargateService = new ecspatterns.ApplicationLoadBalancedFargateService(this, 'CounterFargateService', {
-      cluster: cluster, // Required
+      cluster: cluster,
       memoryLimitMiB: 512,
       desiredCount: 1,
       taskImageOptions: {
         image: ecs.ContainerImage.fromAsset('../back'), // Path to your Dockerfile
-        containerPort: 8080, // 👈 This ensures traffic goes to your container on port 8080
+        containerPort: 8080, // Ensure traffic is directed correctly
       },
-      publicLoadBalancer: true, // Default is false
-      healthCheckGracePeriod: cdk.Duration.seconds(60), // Adjust as needed
-      minHealthyPercent: 100, // Ensure all tasks are healthy before reducing count
-      maxHealthyPercent: 200 // Allow up to double the desired count during deployments
-    });
-
-    // Output the ALB DNS name
-    new cdk.CfnOutput(this, 'LoadBalancerDNS', {
-      value: fargateService.loadBalancer.loadBalancerDnsName,
-      description: 'The DNS name of the load balancer',
+      publicLoadBalancer: true,
+      healthCheckGracePeriod: cdk.Duration.seconds(60),
+      minHealthyPercent: 100,
+      maxHealthyPercent: 200
     });
 
     // Create an S3 bucket for the Vue.js app
     const siteBucket = new s3.Bucket(this, 'CounterSiteBucket', {
       websiteIndexDocument: 'index.html',
-      publicReadAccess: true, // Allow public access to the bucket
+      publicReadAccess: true,
       blockPublicAccess: {
         blockPublicAcls: false,
         blockPublicPolicy: false,
         ignorePublicAcls: false,
         restrictPublicBuckets: false,
       },
-      removalPolicy: cdk.RemovalPolicy.DESTROY, // Automatically delete bucket when stack is deleted
-      autoDeleteObjects: true, // Automatically delete objects in the bucket when stack is deleted
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
     });
 
     // Grant public read access to the bucket
     siteBucket.grantPublicAccess('*', 's3:GetObject');
-   
-    // TODO
-    // siteBucket.grantRead(new iam.AnyPrincipal());
 
     // Create a CloudFront distribution for the S3 bucket
     const distribution = new cloudfront.Distribution(this, 'SiteDistribution', {
@@ -73,13 +63,40 @@ export class CdkStack extends cdk.Stack {
       defaultRootObject: 'index.html',
     });
 
-    // Output the CloudFront distribution domain name
+    // Create an ALB Origin
+    const albOrigin = new LoadBalancerV2Origin(fargateService.loadBalancer, {
+      protocolPolicy: cloudfront.OriginProtocolPolicy.HTTP_ONLY,
+      // protocolPolicy: cloudfront.OriginProtocolPolicy.HTTPS_ONLY,
+      customHeaders: {
+        'X-Forwarded-For': 'CloudFront', // Optional for debugging
+      }
+    });
+
+    // Create a CloudFront Origin Request Policy for APIs
+    const apiOriginRequestPolicy = new cloudfront.OriginRequestPolicy(this, 'ApiOriginRequestPolicy', {
+      queryStringBehavior: cloudfront.OriginRequestQueryStringBehavior.all(),
+      headerBehavior: cloudfront.OriginRequestHeaderBehavior.all(),
+      cookieBehavior: cloudfront.OriginRequestCookieBehavior.all(),
+    });
+
+    // Add a new behavior for /api to route to the ALB
+    distribution.addBehavior('/api/*', albOrigin, {
+      viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+      originRequestPolicy: apiOriginRequestPolicy,
+      allowedMethods: cloudfront.AllowedMethods.ALLOW_ALL, // ✅ Allows GET, POST, PUT, DELETE, etc.
+      cachePolicy: cloudfront.CachePolicy.CACHING_DISABLED, // ✅ Prevents caching issues
+    });    
+
     new cdk.CfnOutput(this, 'CloudFrontURL', {
       value: distribution.distributionDomainName,
       description: 'The URL of the CloudFront distribution',
     });
 
-    // Output the CloudFront distribution ID
+    new cdk.CfnOutput(this, 'LoadBalancerDNS', {
+      value: fargateService.loadBalancer.loadBalancerDnsName,
+      description: 'The DNS name of the load balancer',
+    });
+
     new cdk.CfnOutput(this, 'CloudFrontDistributionId', {
       value: distribution.distributionId,
       description: 'The ID of the CloudFront distribution',
